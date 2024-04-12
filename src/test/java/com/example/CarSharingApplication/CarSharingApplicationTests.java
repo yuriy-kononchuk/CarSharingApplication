adf76ba8630d528d006b7d7b44bab7605a9a0725
Yuriy Kononchuk <y_kononchuk@ukr.net>
2024-04-12 17:03:22 +0300

    Initial commit

---
+++ src/test/java/com/example/CarSharingApplication/CarSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarSharingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
